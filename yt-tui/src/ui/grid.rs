3de2fb76fa27e8ea98be66456b6700b282557223
MathGeniusJodie <mail@jodie.website>
2026-01-27 11:34:47 -0500

    more badges

--- yt-tui/src/ui/grid.rs
+++ yt-tui/src/ui/grid.rs
@@ -84,12 +84,13 @@ impl BoxButton {
 }
 
 fn render_header(frame: &mut Frame, state: &AppState, area: Rect) {
-    let badges: [&str; 22] = [
+    let badges: [&str;_] = [
         "⓿", "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒", "➓", "⓫", "⓬", "⓭", "⓮", "⓯", "⓰", "⓱",
-        "⓲", "⓳", "⓴", "∞",
+        "⓲", "⓳", "⓴", "㉑", "㉒", "㉓", "㉔", "㉕", "㉖", "㉗", "㉘", "㉙", "㉚", "㉛", "㉜", "㉝", "㉞", "㉟",
+        "㊱", "㊲", "㊳", "㊴", "㊵", "㊶", "㊷", "㊸", "㊹", "㊺", "㊻", "㊼", "㊽", "㊾", "㊿", "∞"
     ];
 
-    let watch_later_badge = badges[state.watch_later.len().min(21)];
+    let watch_later_badge = badges[state.watch_later.len().min(badges.len() - 1)];
     let mut watch_later_label = " Watch Later ".to_string();
     watch_later_label.push_str(watch_later_badge);
     watch_later_label.push_str(" ");
